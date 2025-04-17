d6c1fc31c647b1d962f1ad888809a8819109c506
Huilin Deng <huilin_deng@mail.ustc.edu.cn>
2025-04-17 15:25:56 +0800

    Update Test_refgta.sh

--- eval/Test_refgta.sh
+++ eval/Test_refgta.sh
@@ -6,14 +6,14 @@ OUTPUT_DIR='/mnt/tenant-home_speed/dhl/VLM-R1-main/Test/sft_data/v1-20250224-102
 
 
 # 并行运行所有评估任务 八卡并行测试
-python ${BASE_DIR}/src/eval/test_rec_r1.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 100 --gpu 0 --sample_num 1000 &
-python ${BASE_DIR}/src/eval/test_rec_r1.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 200 --gpu 1 --sample_num 1000 &
-python ${BASE_DIR}/src/eval/test_rec_r1.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 300 --gpu 2 --sample_num 1000 &
-python ${BASE_DIR}/src/eval/test_rec_r1.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 400 --gpu 3 --sample_num 1000 &
-python ${BASE_DIR}/src/eval/test_rec_r1.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 500 --gpu 4 --sample_num 1000 &
-python ${BASE_DIR}/src/eval/test_rec_r1.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 600 --gpu 5 --sample_num 1000 &
-python ${BASE_DIR}/src/eval/test_rec_r1.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 700 --gpu 6 --sample_num 1000 &
-python ${BASE_DIR}/src/eval/test_rec_r1.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 1700 --gpu 7 --sample_num 1000 &
+python ${BASE_DIR}/eval/muti_process_eval_for_refgta.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 100 --gpu 0 --sample_num 1000 &
+python ${BASE_DIR}/eval/muti_process_eval_for_refgta.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 200 --gpu 1 --sample_num 1000 &
+python ${BASE_DIR}/eval/muti_process_eval_for_refgta.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 300 --gpu 2 --sample_num 1000 &
+python ${BASE_DIR}/eval/muti_process_eval_for_refgta.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 400 --gpu 3 --sample_num 1000 &
+python ${BASE_DIR}/eval/muti_process_eval_for_refgta.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 500 --gpu 4 --sample_num 1000 &
+python ${BASE_DIR}/eval/muti_process_eval_for_refgta.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 600 --gpu 5 --sample_num 1000 &
+python ${BASE_DIR}/eval/muti_process_eval_for_refgta.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 700 --gpu 6 --sample_num 1000 &
+python ${BASE_DIR}/eval/muti_process_eval_for_refgta.py --BSZ 32 --checkpoint_dir ${CHECKPOINT_DIR} --steps 800 --gpu 7 --sample_num 1000 &
 
 # 等待所有后台任务完成
 wait
